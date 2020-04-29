688bccc3155909ad1d5f05fb95884af395cf7935
Arjun Ponnusamy <arjun@geolang.com>
2020-04-29 22:13:04 +0100

    initial site

---
+++ README.md
@@ -0,0 +1,2 @@
+# nestcam
+RPi
---
+++ assets/js/custom.js
@@ -0,0 +1,29 @@
+var video = document.getElementById("video");
+var videoSrc = "https://email.en.ascema.com/audio/stream.m3u8";
+
+var streaming = false;
+
+function startStream() {
+  if (streaming) {
+    document.getElementById("streamthumbnail").src = "images/nest.jpg";
+    video.pause();
+    streaming = false;
+    return;
+  }
+  document.getElementById("streamthumbnail").src =
+    "http://18.132.31.69/?action=stream";
+  if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      video.play();
+    });
+  } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+    video.src = videoSrc;
+    video.addEventListener("loadedmetadata", function () {
+      video.play();
+    });
+  }
+  streaming = true;
+}
